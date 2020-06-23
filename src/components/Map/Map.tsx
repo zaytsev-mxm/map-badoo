471e954bbeb28e557b8a43ba41498af90c5b2725
Maxim Zaytsev <max.zaytsev@magiclab.co>
2020-06-23 12:23:50 +0300

    Added support of TS and initial component for Map

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Map.css';
+
+const Map = () => {
+    return (
+        <div className="map-root">
+            I am a map
+        </div>
+    );
+};
+
+export default Map;
